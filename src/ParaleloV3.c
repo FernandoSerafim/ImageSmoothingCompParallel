cf70a96282fde447d8f49733dc12014dcbe78b8f
FernandoSerafim <fernando.serafim@unesp.br>
2024-06-04 20:05:59 -0300

    Task | Terceira tentativa de paralelização

---
+++ src/ParaleloV3.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <sys/time.h>
+
+#define image(x, y) pixels[y * width + x]
+#define smooth(x, y) filtered[y * width + x]
+
+typedef struct
+{
+    char red, green, blue, alpha;
+} RGBA;
+
+int main(int argc, char *argv[])
+{
+    struct timeval start, stop;
+    FILE *in, *out;
+    short width, height;
+    RGBA *pixels, *filtered;
+    int DY[] = {-2, -2, -2, -2, -2, -1, -1, -1, -1, -1, +0, +0, +0, +0, +0, +1, +1, +1, +1, +1, +2, +2, +2, +2, +2};
+    int DX[] = {-2, -1, +0, +1, +2, -2, -1, +0, +1, +2, -2, -1, +0, +1, +2, -2, -1, +0, +1, +2, -2, -1, +0, +1, +2};
+    int x, y, d, dx, dy, i;
+    int _;
+
+    in = fopen("image.in", "rb");
+    if (in == NULL)
+    {
+        perror("image.in");
+        exit(EXIT_FAILURE);
+    }
+
+    out = fopen("imageParallelTeste.out", "wb");
+    if (out == NULL)
+    {
+        perror("imageParallelTeste.out");
+        exit(EXIT_FAILURE);
+    }
+
+    _=fread(&width, sizeof(width), 1, in);
+    _=fread(&height, sizeof(height), 1, in);
+    _=fwrite(&width, sizeof(width), 1, out);
+    _=fwrite(&height, sizeof(height), 1, out);
+
+    pixels = (RGBA *)malloc(height * width * sizeof(RGBA));
+    filtered = (RGBA *)malloc(height * width * sizeof(RGBA));
+    if (pixels == NULL || filtered == NULL)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }   
+
+    gettimeofday(&start, NULL);
+
+    do
+    {
+        if (!fread(pixels, height * width * sizeof(RGBA), 1, in))
+            break;
+
+        #pragma omp parallel for private(x, d, dx, dy) schedule(static) 
+        for (y = 0; y < height; y++)
+        {
+            for (x = 0; x < width; x++)
+            {
+                for (i = 0; i < 4; i++)
+                {
+                    long long int sum = 0;
+
+                    for (d = 0; d < 25; d++)
+                    {
+                        dx = x + DX[d];
+                        dy = y + DY[d];
+                        if (dx >= 0 && dx < width && dy >= 0 && dy < height)
+                        {
+                            sum += *(((char *)(&image(dx, dy))) + i);
+                        }
+                    }
+                    (*(((char *)(&smooth(x, y)) + i))) = sum / 25;
+                }
+            }
+        }
+
+        _=fwrite(filtered, height * width * sizeof(RGBA), 1, out);
+    } while (!feof(in));
+
+     gettimeofday(&stop, NULL);
+
+     double t = (((double)(stop.tv_sec)*1000.0  + (double)(stop.tv_usec / 1000.0)) - \
+                   ((double)(start.tv_sec)*1000.0 + (double)(start.tv_usec / 1000.0)));
+
+    fprintf(stdout, "Tempo decorrido = %g ms\n", t);
+
+    free(pixels);
+    free(filtered);
+    fclose(in);
+    fclose(out);
+
+    return EXIT_SUCCESS;
+}
